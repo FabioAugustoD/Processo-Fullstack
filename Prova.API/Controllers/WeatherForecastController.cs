c93e57b6e712a58807c4e00e68567d9e469a93a8
FabioAugustoD <fabioaugustodametto@gmail.com>
2022-04-12 18:37:47 -0300

    Configuração inicial

---
+++ Prova.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prova.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocacaoController : ControllerBase
+    {
+        public LocacaoController()
+        {           
+        }
+
+    }
+}
