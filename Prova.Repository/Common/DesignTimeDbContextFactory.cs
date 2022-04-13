f970c4368b864f6d75dcb77eee7f9083420db01a
FabioAugustoD <fabioaugustodametto@gmail.com>
2022-04-13 19:26:09 -0300

    Preparando Migrations

---
+++ Prova.Repository/Common/DesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Prova.Repository
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var filename = Directory.GetCurrentDirectory() + $"/../Prova.API/appsettings.{env}.json";
+
+            var config = new ConfigurationBuilder().AddJsonFile(filename).Build();
+            var connectionString = config.GetConnectionString("App");
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer(connectionString);
+            
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
